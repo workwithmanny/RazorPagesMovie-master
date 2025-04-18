867e3f8e6cd3e7264f2045341e34c2865df87d30
workwithmanny <Adeniyiemmanuel896@gmail.com>
2025-04-18 14:45:36 +0100

    Movie App Set

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+// if (builder.Environment.IsDevelopment())
+// {
+//     builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+//         options.UseSqlite(builder.Configuration.GetConnectionString("RazorPagesMovieContext")));
+// }
+// else
+// {
+//     builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+//         options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMovieContext")));
+// }
 
 var app = builder.Build();
 

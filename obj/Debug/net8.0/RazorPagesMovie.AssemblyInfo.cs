31b2c7836d1dbf8ae48557c056e596d60e7c3489
workwithmanny <Adeniyiemmanuel896@gmail.com>
2025-04-18 14:47:41 +0100

    Movie App Set

--- obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9739422ac16420d907a6a36559310b9e3a909e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867e3f8e6cd3e7264f2045341e34c2865df87d30")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
